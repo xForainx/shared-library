fd9f892c29d41ea690349b8be9b39abbb13f232e
xForainx <80107388+xForainx@users.noreply.github.com>
2024-02-19 14:27:05 +0100

    Create slackNotifier.groovy

---
+++ vars/slackNotifier.groovy
@@ -0,0 +1,153 @@
+pipeline {
+    agent none
+    environment {
+        DOCKERHUB_AUTH = credentials('e7a43fd0-0579-4387-83c4-01d4483a3002')
+        ID_DOCKER = "${DOCKERHUB_AUTH_USR}"
+        PORT_EXPOSED = "80"
+        HOSTNAME_DEPLOY_STAGING = "ec2-54-87-152-121.compute-1.amazonaws.com"
+        HOSTNAME_DEPLOY_PROD = "ec2-107-22-94-24.compute-1.amazonaws.com"
+    }
+
+    stages {
+        stage ('Build image') {
+            agent any
+            steps {
+                script {
+                    sh 'docker build -t ${ID_DOCKER}/${IMAGE_NAME}:${IMAGE_TAG} .'
+                }
+            }
+        }
+
+        stage('Run container based on builded image') {
+            agent any
+            steps {
+               script {
+                 sh '''
+                    echo "Clean Environment"
+                    docker rm -f $IMAGE_NAME || echo "container does not exist"
+                    docker run --name $IMAGE_NAME -d -p ${PORT_EXPOSED}:5000 -e PORT=5000 ${ID_DOCKER}/$IMAGE_NAME:$IMAGE_TAG
+                    sleep 5
+                 '''
+               }
+            }
+        }
+
+        stage('Test image') {
+            agent any
+            steps {
+                script {
+                    sh '''
+                        curl http://172.17.0.1:${PORT_EXPOSED} | grep -q "Hello world Sylvain!"
+                    '''
+                }
+            }
+        }
+ 
+        stage('Clean Container') {
+            agent any
+            steps {
+                script {
+                sh '''
+                    docker stop $IMAGE_NAME
+                    docker rm $IMAGE_NAME
+                '''
+                }
+            }
+        }
+
+        stage ('Login and Push Image on docker hub') {
+            agent any           
+            steps {
+                script {
+                    sh '''
+                        docker login -u $DOCKERHUB_AUTH_USR -p $DOCKERHUB_AUTH_PSW
+                        docker push ${ID_DOCKER}/$IMAGE_NAME:$IMAGE_TAG
+                    '''
+                }
+            }
+        }
+
+        stage ('Deploy in staging') {
+            agent any
+            // La variable a été déplacée dans les variables globales (en haut du fichier)
+            // environment {
+            //     HOSTNAME_DEPLOY_STAGING = "ec2-54-87-152-121.compute-1.amazonaws.com"
+            // }
+            steps {
+                sshagent(credentials: ['SSH_AUTH_SERVER']) {
+                    sh '''
+                        [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
+                        ssh-keyscan -t rsa,dsa ${HOSTNAME_DEPLOY_STAGING} >> ~/.ssh/known_hosts
+                        command1="docker login -u $DOCKERHUB_AUTH_USR -p $DOCKERHUB_AUTH_PSW"
+                        command2="docker pull $DOCKERHUB_AUTH_USR/$IMAGE_NAME:$IMAGE_TAG"
+                        command3="docker rm -f webapp || echo 'container does not exist'"
+                        command4="docker run -d -p 80:5000 -e PORT=5000 --name webapp $DOCKERHUB_AUTH_USR/$IMAGE_NAME:$IMAGE_TAG"
+                        ssh -t centos@${HOSTNAME_DEPLOY_STAGING} \
+                            -o SendEnv=IMAGE_NAME \
+                            -o SendEnv=IMAGE_TAG \
+                            -o SendEnv=DOCKERHUB_AUTH_USR \
+                            -o SendEnv=DOCKERHUB_AUTH_PSW \
+                            -C "$command1 && $command2 && $command3 && $command4"
+                    '''
+                }
+            }
+        }
+
+        stage('Test Staging') {
+            agent any
+            steps {
+              script {
+                sh '''
+                  curl ${HOSTNAME_DEPLOY_STAGING} | grep -q "Hello world Sylvain!"
+                '''
+              }
+            }
+        }
+
+        stage ('Deploy in prod') {
+            agent any
+            // environment {
+            //     HOSTNAME_DEPLOY_PROD = "ec2-107-22-94-24.compute-1.amazonaws.com"
+            // }
+            steps {
+                sshagent(credentials: ['SSH_AUTH_PROD']) {
+                    sh '''
+                        [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
+                        ssh-keyscan -t rsa,dsa ${HOSTNAME_DEPLOY_PROD} >> ~/.ssh/known_hosts
+                        command1="docker login -u $DOCKERHUB_AUTH_USR -p $DOCKERHUB_AUTH_PSW"
+                        command2="docker pull $DOCKERHUB_AUTH_USR/$IMAGE_NAME:$IMAGE_TAG"
+                        command3="docker rm -f webapp || echo 'container does not exist'"
+                        command4="docker run -d -p 80:5000 -e PORT=5000 --name webapp $DOCKERHUB_AUTH_USR/$IMAGE_NAME:$IMAGE_TAG"
+                        ssh -t centos@${HOSTNAME_DEPLOY_PROD} \
+                            -o SendEnv=IMAGE_NAME \
+                            -o SendEnv=IMAGE_TAG \
+                            -o SendEnv=DOCKERHUB_AUTH_USR \
+                            -o SendEnv=DOCKERHUB_AUTH_PSW \
+                            -C "$command1 && $command2 && $command3 && $command4"
+                    '''
+                }
+            }
+        }
+
+                stage('Test Prod') {
+            agent any
+            steps {
+              script {
+                sh '''
+                  curl ${HOSTNAME_DEPLOY_STAGING} | grep -q "Hello world Sylvain!"
+                '''
+              }
+            }
+        }
+
+    }
+
+    post {
+        success {
+            slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+        }
+        failure {
+            slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+        }
+    }
+}
